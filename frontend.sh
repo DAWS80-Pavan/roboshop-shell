2cba518590e0f5e1463c830710513158e43987d6
pava <pavareddy1030@gmail.com>
2024-07-13 20:08:18 +0530

    removing nginx

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,11 @@
-dnf module disable nginx -y
-dnf module enable nginx:1.24 -y
-dnf install nginx -y
-rm -rf /usr/share/nginx/html/*
-cp ngix.conf /etc/nginx/nginx.conf
-systemctl enable nginx
-systemctl start nginx
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-systemctl restart nginx
+#dnf module disable nginx -y
+#dnf module enable nginx:1.24 -y
+dnf remove nginx -y
+#rm -rf /usr/share/nginx/html/*
+#cp ngix.conf /etc/nginx/nginx.conf
+#systemctl enable nginx
+#systemctl start nginx
+#curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+#cd /usr/share/nginx/html
+#unzip /tmp/frontend.zip
+#systemctl restart nginx
